200142a94dbdc91d89335f5b1e850657274a5605
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-21 11:35:29 -0500

    Add (Function) binary_tree_node Changes to be committed: 	new file:   .gitignore 	modified:   0-binary_tree_node.c 	new file:   binary_tree_print.c 	modified:   binary_trees.h 	new file:   main-files/0-main.c

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,1 +1,28 @@
-#include <"binary_trees.h">
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - Function that creates a binary tree node.
+ *
+ * @parent: Pointer to the parent node of the node to create.
+ * @value: The value to put in the new node.
+ *
+ * Return: Return a pointer to the new node, or NULL on failure.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = malloc(sizeof(binary_tree_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
