b5f877bce80f0ee4bdfc730c456bb9b149a11e8a
cristhian1107 <cristhian.cjaa@gmail.com>
2022-03-22 18:27:52 -0500

    Modify (Function) binary_tree_node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -14,7 +14,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node = NULL;
 
 	new_node = malloc(sizeof(binary_tree_t));
-
 	if (!new_node)
 		return (NULL);
 
