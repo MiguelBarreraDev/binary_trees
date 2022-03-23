15d6a6cb373f3ff86f4e9ce27ed4afb524334b32
MiguelBarreraDev <barreram987@gmail.com>
2022-03-23 14:36:27 -0500

    Update: Use of MACROS to get max between two values

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,18 +1,28 @@
 #include "binary_trees.h"
+#include <stddef.h>
 
+#define MAX(v1, v2) ((v1 >= v2) ? (v1) : (v2))
 
 /**
- * binary_tree_height - Function that measures the height of a binary tree.
+ * binary_tree_height - function that measures the height of a binary tree.
+ * If tree is NULL, your function must return 0.
  *
- * @tree: Pointer to the root node of the tree to measure the height.
+ * @tree: Is a pointer to the root node of the tree to measure the height.
  *
- * Return: The height of a binary tree de bottom-up.
+ * Return: The height of the tree.
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree->left)
-		return (binary_tree_height(tree->left) + 1);
-	if (tree->right)
-		return (binary_tree_height(tree->right) + 1);
-	return (0);
+	size_t left_height = 0, right_height = 0;
+
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return (1 + MAX(left_height, right_height));
 }
